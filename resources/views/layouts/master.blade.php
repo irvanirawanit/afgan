2ed7f62eeeff30bacf287c96615ec38a51134842
irvan <irvanirawanit@gmail.com>
2021-03-28 16:55:08 +0700

    tes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Starter</title>
+  <title>V A N</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -261,12 +261,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">@yield('title')</h1>
+            <h1 class="m-0">@yield('header')</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               {{-- <li class="breadcrumb-item"><a href="#">Home</a></li> --}}
-              <li class="breadcrumb-item active">@yield('title')</li>
+              <li class="breadcrumb-item active">@yield('header')</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.bootstrap4.min.css">
 @endsection
 
-@section('title', 'Base Cylinder')
+@section('header', 'Base Cylinder')
 
 @section('content')
 <div class="container-fluid">
@@ -132,7 +132,7 @@
 <script>
     $(function () {
       $("#example1").DataTable({
-        "responsive": true, "lengthChange": false, "autoWidth": false,"iDisplayLength": 100,
+        "responsive": true, "lengthChange": true, "autoWidth": false,"iDisplayLength": 100,
         "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
       }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     });
